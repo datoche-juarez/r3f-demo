f60a22d5cef90faf0db1c492eca5fad0db62db4f
Daniel Atoche-Juarez <dtoch123@gmail.com>
2025-04-07 18:07:05 -0400

    We now have a working model with lighting environment set up.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { Canvas } from "@react-three/fiber";
+import {
+  OrbitControls,
+  useGLTF,
+  ContactShadows,
+  Environment,
+} from "@react-three/drei";
+import { GridHelper } from "three";
+import { useThree } from "@react-three/fiber";
 
-function Box() {
-  return (
-    <mesh rotation={[0.4, 0.2, 0]}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="orange" />
-    </mesh>
-  )
+useGLTF.preload("/models/product.glb");
+
+function Model() {
+  const gltf = useGLTF("/models/product.glb");
+  return <primitive object={gltf.scene} scale={40} />;
+}
+
+function GroundGrid() {
+  const { scene } = useThree();
+
+  const grid = new GridHelper(20, 20);
+  grid.material.depthWrite = false;
+  scene.add(grid);
+
+  return null;
 }
 
 export default function App() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <Canvas camera={{ position: [2, 2, 2] }}>
-        <ambientLight />
-        <directionalLight position={[3, 3, 3]} />
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas camera={{ position: [0, 3, 8], fov: 35 }}>
+        <Environment preset="warehouse" />
         <OrbitControls />
-        <Box />
+        <GroundGrid />
+        <ContactShadows
+          position={[0, -1.2, 0]}
+          opacity={0.4}
+          scale={10}
+          blur={2.5}
+        />
+        <Model />
       </Canvas>
     </div>
-  )
+  );
 }
