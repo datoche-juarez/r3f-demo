326ea85d9315d14d26d5b647c4d0da957a64ece8
Daniel Atoche-Juarez <dtoch123@gmail.com>
2025-04-08 18:49:18 -0400

    Tweaked height issues for mobile.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,19 +3,17 @@ import ProductDetails from "./ProductDetails";
 
 export const Product = () => {
   return (
-    <div className="flex flex-col w-full h-full gap-4 md:flex-row">
+    <div className="flex flex-col w-full gap-4 md:flex-row">
       {/* Left: 3D Model Viewer */}
-      <div className="flex w-full md:w-1/2">
-        <div className="relative flex flex-col justify-center w-full h-full bg-product-bg">
-          <ProductModel />
-          <p className="absolute z-10 flex justify-center w-full px-2 text-sm transform -translate-x-1/2 bottom-2 left-1/2 text-text-main">
-            Click & drag to rotate • Scroll to zoom
-          </p>
-        </div>
+      <div className="relative w-full bg-product-bg aspect-[5/3]  md:w-1/2 md:h-auto flex items-center justify-center">
+        <ProductModel />
+        <p className="absolute z-10 flex justify-center w-full px-2 text-sm transform -translate-x-1/2 bottom-2 left-1/2 text-text-main">
+          Click & drag to rotate • Scroll to zoom
+        </p>
       </div>
 
       {/* Right: Product Info */}
-      <div className="flex items-center justify-center w-full p-8 md:w-1/2 bg-product-bg">
+      <div className="flex items-center justify-center w-full p-8 bg-product-bg md:w-1/2">
         <ProductDetails />
       </div>
     </div>
