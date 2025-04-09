a8944d30d62e8615e2536609efb6e73cfd7c82e5
Daniel Atoche-Juarez <dtoch123@gmail.com>
2025-04-09 18:57:44 -0400

    Added PWA integration.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // Optional prompt
+  },
+  onOfflineReady() {
+    // Optional offline support ready prompt
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
